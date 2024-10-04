30f5242866f22841389cca03c8f9117ff04588da
Micheil <fouriemicheil1@gmail.com>
2024-10-04 21:39:07 +0200

    images prep commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
-import hero from './assets/hero.jpg'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { useRef } from 'react'
+import imgParallaxBack from './assets/parallax-back.png'
+import imgParallaxMid from './assets/parallax-mid.png'
+import imgParallaxFront from './assets/parallax-fore.png'
 
 function App() {
   return (
-    <main className='relative'>
-      <img className='absolute -z-50 w-full h-full object-cover' src={hero} alt="" />
+    <main>
+      <Parallax pages={2} style={{ top: 0, left: 0, width: '100%', height: '100%' }}>
+        <ParallaxLayer
+          offset={0}
+          speed={-0.05}
+        // style={{
+        //   backgroundImage: `url(${imgParallaxBack})`,
+        //   backgroundSize: 'cover',
+        //   backgroundPosition: 'center'
+        // }}
+        >
 
-    </main>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={0}
+          speed={-0.075}
+          style={{
+            backgroundImage: `url(${imgParallaxMid})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            overflow: 'hidden'
+          }}>
+
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          style={{
+            backgroundImage: `url(${imgParallaxFront})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}>
+
+        </ParallaxLayer>
+      </Parallax>
+
+    </main >
   )
 }
 
